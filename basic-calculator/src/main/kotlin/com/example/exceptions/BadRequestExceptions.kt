dea9769f00e7964a40510f8197b365899628225a
Pe-Guedss <pedro.og2002@gmail.com>
2023-01-26 00:31:22 -0300

    Fix: Fixing the super class instantiation.

--- basic-calculator/src/main/kotlin/com/example/exceptions/BadRequestExceptions.kt
+++ basic-calculator/src/main/kotlin/com/example/exceptions/BadRequestExceptions.kt
@@ -1,6 +1,4 @@
 package com.example.exceptions
 
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-
-class BadRequestExceptions(exception: String? = "There is an unspecified error in the request. Check it and try again!"):
-    RuntimeException()
+class BadRequestExceptions(exception: String? = "There is an unspecified error in the request. Check it and try again!",):
+    RuntimeException(exception)
