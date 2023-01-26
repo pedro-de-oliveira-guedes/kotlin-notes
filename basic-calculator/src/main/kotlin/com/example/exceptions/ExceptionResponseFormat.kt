66d7c4af390db61630b95a59b2b158674dd698b2
Pe-Guedss <pedro.og2002@gmail.com>
2023-01-26 00:32:23 -0300

    Ref: Changing the function declaration format.

--- basic-calculator/src/main/kotlin/com/example/exceptions/ExceptionResponseFormat.kt
+++ basic-calculator/src/main/kotlin/com/example/exceptions/ExceptionResponseFormat.kt
@@ -2,4 +2,7 @@ package com.example.exceptions
 
 import java.util.Date
 
-class ExceptionResponseFormat(val timestamp: Date, val message: String?, val details: String?)
+class ExceptionResponseFormat(
+    val timestamp: Date = Date(),
+    val message: String? = "Error!",
+    val details: String? = "An error occurred!",)
