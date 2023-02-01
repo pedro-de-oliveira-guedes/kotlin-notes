9c567613bbf9c9db2604127bf324e6dc414d350e
Pe-Guedss <pedro.og2002@gmail.com>
2023-02-01 01:53:58 -0300

    Feat: Creating an ID auto incrementer. Feat: Defining a system logger. Feat: Defining the PersonServices class as a Service with Spring.

--- db-mock/src/main/kotlin/com/example/services/person/PersonServices.kt
+++ db-mock/src/main/kotlin/com/example/services/person/PersonServices.kt
@@ -1,9 +1,20 @@
 package com.example.services.person
 
 import com.example.models.person.Person
+import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicLong
+import java.util.logging.Logger
 
+@Service
 class PersonServices {
+
+    private val sysLogger: Logger = Logger.getLogger(PersonServices::class.java.name)
+
+    private val autoId: AtomicLong = AtomicLong()
+
     fun getById(id: Long): Person {
+        sysLogger.info("Searching person with the provided ID!")
+
         val returnedPerson = Person(id, "Mockerson Mockelson de Mockers", 17, "Mooocked Street, Mockest Wirginia, Mockers")
 
         return returnedPerson
