321e27dd88e5a10999f03ab6ceced1990f350373
Pe-Guedss <pedro.og2002@gmail.com>
2023-02-01 02:24:24 -0300

    Feat: Returning a person object from the getter defined.

---
+++ db-mock/src/main/kotlin/com/example/controller/personController.kt
@@ -0,0 +1,23 @@
+package com.example.controller
+
+import com.example.models.person.Person
+import com.example.services.person.PersonServices
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/person")
+class personController {
+
+    @Autowired
+    private lateinit var services: PersonServices
+
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun test(@PathVariable(value = "id") id: Long): Person {
+        return services.getById(id)
+    }
+}
