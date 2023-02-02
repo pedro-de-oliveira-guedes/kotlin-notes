949a3e62777aa153d70bdcbfadd7448236fda906
Pe-Guedss <pedro.og2002@gmail.com>
2023-02-02 00:41:03 -0300

    Feat: Defining a class to explicit the exception response format.

---
+++ db-mock/src/main/kotlin/com/example/custom_exceptions/ExceptionResponseFormat.kt
@@ -0,0 +1,10 @@
+package com.example.custom_exceptions
+
+import java.sql.Timestamp
+import java.util.Date
+
+class ExceptionResponseFormat (
+    timestamp: Date = Date(),
+    message: String = "An error occurred.",
+    details: String = "Error!"
+)
