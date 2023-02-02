d2ab8ce7eacd327cf882c3606047e76873956378
Pe-Guedss <pedro.og2002@gmail.com>
2023-02-02 00:57:06 -0300

    Feat: Defining the error handler for the application.

---
+++ db-mock/src/main/kotlin/com/example/custom_exceptions/ExceptionHandler.kt
@@ -0,0 +1,34 @@
+package com.example.custom_exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.util.*
+
+@ControllerAdvice
+@RestController
+class ExceptionHandler : ResponseEntityExceptionHandler(){
+    @ExceptionHandler(Exception::class)
+    fun serverSideErrors(exception: java.lang.Exception, request: WebRequest): ResponseEntity<ExceptionResponseFormat> {
+        val exceptionResponse = ExceptionResponseFormat(
+            message = exception.message,
+            details = request.getDescription(false),
+        )
+
+        return ResponseEntity<ExceptionResponseFormat>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(BadRequestExcept::class)
+    fun clientSideErrors(exception: BadRequestExcept, request: WebRequest): ResponseEntity<ExceptionResponseFormat> {
+        val exceptionResponse = ExceptionResponseFormat(
+            message = exception.message,
+            details = request.getDescription(false),
+        )
+
+        return ResponseEntity<ExceptionResponseFormat>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
